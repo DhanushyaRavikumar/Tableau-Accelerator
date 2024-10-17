5d0dfa9d715c83531887562a543d3d17fb02ac64
Nagasundaramoorthy2023 <132643280+Nagasundaramoorthy2023@users.noreply.github.com>
2024-10-17 15:41:32 +0530

    Cors

--- app2.py
+++ app2.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 import xml.etree.ElementTree as ET
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 def upload_file():
